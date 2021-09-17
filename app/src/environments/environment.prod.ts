fff6af02dd12dd25688abf52e77a49651c248845
ayoub ar <ayoubarahmat9@gmail.com>
2021-09-17 17:20:56 +0100

    update environment.prod.ts file

--- app/src/environments/environment.prod.ts
+++ app/src/environments/environment.prod.ts
@@ -1,7 +1,8 @@
 export const environment = {
   production: true,
   mapbox:
+    process.env.mapbox ||
     'pk.eyJ1IjoiYXlvdWJhcmFobWF0IiwiYSI6ImNrdG02b2VvdDBqcjUyb24ydWNoaWVyNngifQ.MLtaiRs7xxBpqTFYUE0zCg',
   username: 'ayoubarahmat',
-  API_URL: 'http://127.0.0.1:8000/v0',
+  API_URL: 'https://mapbox-m.herokuapp.com/v0',
 };
